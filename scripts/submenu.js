65e9e68daa5b332767c8e3c560fa7fb147b04b81
laura <laura.gisela.g@gmail.com>
2022-01-10 10:49:42 -0300

    Menu Lateral terminado

--- scripts/submenu.js
+++ scripts/submenu.js
@@ -1,8 +1,17 @@
 window.addEventListener("load", () => {
     let lateral = document.getElementsByClassName('lateral')[0]
     let open_submenu = document.createElement('div')
-    open_submenu.textContent = '>'
+    open_submenu.textContent = '<'
     open_submenu.className = 'lateral-open'
     lateral.insertAdjacentElement('beforebegin', open_submenu)
+    open_submenu.onclick = () => {
+        lateral.classList.toggle('lateralx')
+        open_submenu.classList.toggle('lateral-close')
+        if (open_submenu.className == 'lateral-open lateral-close') {
+            open_submenu.textContent = '>'
+        } else {
+            open_submenu.textContent = '<'
+        }
+    }
 
 })
